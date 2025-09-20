863e847ed64e04c236ff782b4d91b616ce07d4de
karthikambaragonda <ambaragondakarthik@gmail.com>
2025-09-20 19:52:52 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+from db import setup_database
+from service import BankService
+
+def main():
+    setup_database()
+    bank = BankService()
+
+    while True:
+        print("\n🏦 Bank Management System")
+        print("1. Create Account")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Check Balance")
+        print("5. Exit")
+
+        choice = input("Enter choice: ")
+
+        if choice == "1":
+            name = input("Name: ")
+            email = input("Email: ")
+            phone = input("Phone: ")
+            password = input("Password: ")
+            bank.create_account(name, email, phone, password)
+
+        elif choice == "2":
+            acc_id = int(input("Account ID: "))
+            amt = float(input("Amount: "))
+            bank.deposit(acc_id, amt)
+
+        elif choice == "3":
+            acc_id = int(input("Account ID: "))
+            amt = float(input("Amount: "))
+            bank.withdraw(acc_id, amt)
+
+        elif choice == "4":
+            acc_id = int(input("Account ID: "))
+            bank.check_balance(acc_id)
+
+        elif choice == "5":
+            print("👋 Goodbye!")
+            break
+        else:
+            print("❌ Invalid choice.")
+
+if __name__ == "__main__":
+    main()
